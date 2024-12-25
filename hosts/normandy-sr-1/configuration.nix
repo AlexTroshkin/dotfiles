f60b83114c4f202a2e464f0fb97948cab9065846
Alex Troshkin <alextorshkin@outlook.com>
2024-12-25 04:48:11 +0300

    0cf1c697-9c53-4e79-8a32-2cf084b6fb3b

--- hosts/normandy-sr-1/configuration.nix
+++ hosts/normandy-sr-1/configuration.nix
@@ -5,6 +5,8 @@
 {
   config,
   pkgs,
+  lib,
+  modulesPath,
   username,
   ...
 }:
@@ -141,10 +143,12 @@
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
 
       substituters = [
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
       ];
     };
 
